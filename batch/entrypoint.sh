0b7e45676a21170346cc3d65cca1ba7204f6c28a
tmneetse <the024and025@gmail.com>
2020-04-03 16:49:09 +0900

    [CHANGE]バッチ起動シェルの記載を変更

--- batch/entrypoint.sh
+++ batch/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-sleep 30
+while :
+do
+  health="$(curl -fsSL "elasticsearch:${ES_PORT}/_cat/health?h=status")"
+  if [ "$health" = "green" ]; then
+    break
+  fi
+  sleep 1
+done
+
 java -jar -Dspring.profiles.active=docker build/libs/${JAR_TARGET}
 
