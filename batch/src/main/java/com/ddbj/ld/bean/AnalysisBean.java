872c0301e76fca7cec63cd2946990c76dc4ac10c
tmneetse <the024and025@gmail.com>
2020-05-28 23:38:07 +0900

    [UPDATE]Beanの項目の順序

--- batch/src/main/java/com/ddbj/ld/bean/AnalysisBean.java
+++ batch/src/main/java/com/ddbj/ld/bean/AnalysisBean.java
@@ -8,12 +8,6 @@ import java.util.List;
 public class AnalysisBean {
     private String identifier;
 
-    private String dateCreated;
-
-    private String dateModified;
-
-    private String datePublished;
-
     private String name;
 
     private String title;
@@ -24,5 +18,11 @@ public class AnalysisBean {
 
     @JsonRawValue
     private String properties;
+
+    private String dateCreated;
+
+    private String dateModified;
+
+    private String datePublished;
 }
 
--- batch/src/main/java/com/ddbj/ld/bean/SubmissionBean.java
+++ batch/src/main/java/com/ddbj/ld/bean/SubmissionBean.java
@@ -8,12 +8,6 @@ import java.util.List;
 public class SubmissionBean {
     private String identifier;
 
-    private String dateCreated;
-
-    private String dateModified;
-
-    private String datePublished;
-
     private String name;
 
     private String title;
@@ -24,5 +18,11 @@ public class SubmissionBean {
 
     @JsonRawValue
     private String properties;
+
+    private String dateCreated;
+
+    private String dateModified;
+
+    private String datePublished;
 }
 
