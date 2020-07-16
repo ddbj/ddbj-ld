0848ed54cedc73bdc6c0f64f495997b36089f0e9
tmneetse <the024and025@gmail.com>
2020-07-17 03:52:14 +0900

    [UPDATE]JGAのDaoのTransactionalの位置とリターン値を変更

--- batch/src/main/java/com/ddbj/ld/dao/jga/JgaDateDao.java
+++ batch/src/main/java/com/ddbj/ld/dao/jga/JgaDateDao.java
@@ -3,24 +3,22 @@ package com.ddbj.ld.dao.jga;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Repository
+@Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
 @AllArgsConstructor
 @Slf4j
 public class JgaDateDao {
     private JdbcTemplate jdbcTemplate;
 
-    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
     public int[] bulkInsert(List<Object[]> recordList) {
         int[] argTypes = new int[4];
         argTypes[0] = Types.VARCHAR;
@@ -43,29 +41,15 @@ public class JgaDateDao {
         }
     }
 
-    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
-    public List<String[]> selJgaDate(String accession) {
-        String sql = "select * from jga_date where accession = ?";
+    public Map<String, Object> selJgaDate(String accession) {
+        final var sql = "select * from jga_date where accession = ?";
 
         jdbcTemplate.setFetchSize(1000);
 
-        List<String[]> jgaDateList = jdbcTemplate.query(sql, new Object[]{ accession }, new RowMapper<String[]>() {
-            public String[] mapRow(ResultSet rs, int rowNum) {
-                try {
-                    String[] dateArr = new String[3];
-                    dateArr[0] = rs.getString("date_created");
-                    dateArr[1] = rs.getString("date_published");
-                    dateArr[2] = rs.getString("date_modified");
+        Object[] args = {
+                accession
+        };
 
-                    return dateArr;
-                } catch (SQLException e) {
-                    log.debug(e.getMessage());
-
-                    return null;
-                }
-            }
-        });
-
-        return jgaDateList;
+        return jdbcTemplate.queryForMap(sql, args);
     }
 }
