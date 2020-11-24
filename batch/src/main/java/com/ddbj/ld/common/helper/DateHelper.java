ab72b370756dc22c5bfa6a44c975ae3fb9ef0970
KojiIshikura <59942296+KojiIshikura@users.noreply.github.com>
2020-11-24 17:28:08 +0900

    変更分取り込み

--- batch/src/main/java/com/ddbj/ld/common/helper/DateHelper.java
+++ batch/src/main/java/com/ddbj/ld/common/helper/DateHelper.java
@@ -18,8 +18,9 @@ public class DateHelper {
     private SimpleDateFormat  esSimpleDateFormat;
     private DateTimeFormatter esFormatter;
 
-    private SimpleDateFormat  esSimpleDateFormat;
-    private DateTimeFormatter esFormatter;
+    public DateHelper() {
+        this.esSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZ");
+    }
 
     public String parse(Timestamp timestamp) {
         try {
