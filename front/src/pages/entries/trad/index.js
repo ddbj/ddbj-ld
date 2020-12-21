b45cab353a1da5d18c7b8ffdc6643b7c7608b819
tmneetse <the024and025@gmail.com>
2020-12-21 15:13:33 +0900

    [UPDATE]JVar以外のDBのページを工事中にした

--- front/src/pages/entries/trad/index.js
+++ front/src/pages/entries/trad/index.js
@@ -4,11 +4,14 @@ import List from './List'
 import Edit from './edit'
 
 const  Entries = () => (
-    <Switch>
-        <Route path="/entries/trad/:uuid" component={Edit}/>
-        <Route path="/entries/trad" component={List}/>
-        <Redirect to="/search"/>
-    </Switch>
+    <>
+        Under Construction...
+    </>
+    // <Switch>
+    //     <Route path="/entries/trad/:uuid" component={Edit}/>
+    //     <Route path="/entries/trad" component={List}/>
+    //     <Redirect to="/search"/>
+    // </Switch>
 )
 
 export default Entries
