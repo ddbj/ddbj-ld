b5d6f85bb5a8d6bdb7b8eda8550dd039e00f7938
tmneetse <the024and025@gmail.com>
2021-02-22 17:54:36 +0900

    [FIX]BioProjectのエラーで落ちている部分を一部修正

--- batch/src/main/java/com/ddbj/ld/data/beans/bioproject/PeerProject.java
+++ batch/src/main/java/com/ddbj/ld/data/beans/bioproject/PeerProject.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.*;
 public class PeerProject {
     private MemberID memberID;
     private String commonInputData;
+    private String bioticRelationship;
 
     @JsonProperty("MemberID")
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -19,4 +20,12 @@ public class PeerProject {
     @JsonProperty("CommonInputData")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public void setCommonInputData(String value) { this.commonInputData = value; }
+
+    @JsonProperty("BioticRelationship")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public String getBioticRelationship() { return bioticRelationship; }
+    @JsonProperty("BioticRelationship")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public void setBioticRelationship(String value) { this.bioticRelationship = value; }
+
 }
