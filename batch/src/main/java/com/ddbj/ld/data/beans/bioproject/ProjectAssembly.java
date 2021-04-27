8721174acace7df7a4d0e5eb824e24416810048c
KojiIshikura <59942296+KojiIshikura@users.noreply.github.com>
2021-04-27 19:36:01 +0900

    [UPDATE]その他機能の一部修正

---
+++ batch/src/main/java/com/ddbj/ld/data/beans/bioproject/ProjectAssembly.java
@@ -0,0 +1,24 @@
+package com.ddbj.ld.data.beans.bioproject;
+
+import com.fasterxml.jackson.annotation.*;
+
+public class ProjectAssembly {
+    private String typeOfPairing;
+    private Assembly assembly;
+    private ArchiveID projectIDRef;
+
+    @JsonProperty("typeOfPairing")
+    public String getTypeOfPairing() { return typeOfPairing; }
+    @JsonProperty("typeOfPairing")
+    public void setTypeOfPairing(String value) { this.typeOfPairing = value; }
+
+    @JsonProperty("Assembly")
+    public Assembly getAssembly() { return assembly; }
+    @JsonProperty("Assembly")
+    public void setAssembly(Assembly value) { this.assembly = value; }
+
+    @JsonProperty("ProjectIDRef")
+    public ArchiveID getProjectIDRef() { return projectIDRef; }
+    @JsonProperty("ProjectIDRef")
+    public void setProjectIDRef(ArchiveID value) { this.projectIDRef = value; }
+}
