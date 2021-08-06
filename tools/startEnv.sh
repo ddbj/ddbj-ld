c7644fed43ddd3158d0184b4feeaa6c653929dc4
tmneetse <the024and025@gmail.com>
2021-08-06 17:40:44 +0900

    [UPDATE]startEnv.shを現状のコンテナ構成に更新

--- tools/startEnv.sh
+++ tools/startEnv.sh
@@ -4,12 +4,24 @@
 
 Env=$1
 ESPort=9200
+PUBLIC_DB=public_db
+JVAR_DB=jvar_db
+FRONT=front
+API=api
+ELASTIC_SEARCH_1=elasticsearch
+ELASTIC_SEARCH_2=elasticsearch2
 
 if [ "$Env" = "stage" ]; then
- ESPort=9202
+  ESPort=9202
+  PUBLIC_DB=public_db_stage
+  JVAR_DB=jvar_db_stage
+  FRONT=front_stage
+  API=api_stage
+  ELASTIC_SEARCH_1=elasticsearch_stage
+  ELASTIC_SEARCH_2=elasticsearch2_stage
 fi
 
-docker-compose up -d postgresql elasticsearch elasticsearch2
+docker-compose up -d $PUBLIC_DB $JVAR_DB $FRONT $API $ELASTIC_SEARCH_1 $ELASTIC_SEARCH_2
 
 while :
 do
