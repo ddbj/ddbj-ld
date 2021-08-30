16ac0138666581aa4ee45f73f8fb61245c16b275
tmneetse <the024and025@gmail.com>
2021-08-30 12:58:09 +0900

    [UPDATE]Elasticsearchのシェルから叩くcurlが標準出力しないようにした

--- docker/elasticsearch/entrypoint.sh
+++ docker/elasticsearch/entrypoint.sh
@@ -15,7 +15,7 @@ done
 echo "Start to input initial data."
 # shardとreplica数を変更
 curl -H "Content-Type: application/json" -XPUT localhost:9200/_template/default -d '{ "template": ["*"], "index_patterns": ["*"], "order": -1, "settings": {"number_of_shards": "72",
-"number_of_replicas": "2"}}'
+"number_of_replicas": "2"}}' > /dev/null
 
 # TODO 必要に応じてAPIを使用しないと設定できない設定を追加していく
 
