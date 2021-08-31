08483e8042563d02d2c50888796eb48668ef77c9
tmneetse <the024and025@gmail.com>
2021-08-31 18:26:20 +0900

    [UPDATE]Beanを生成するメソッドを3つ追加

--- batch/src/main/java/com/ddbj/ld/app/core/module/JsonModule.java
+++ batch/src/main/java/com/ddbj/ld/app/core/module/JsonModule.java
@@ -3,11 +3,15 @@ package com.ddbj.ld.app.core.module;
 import com.ddbj.ld.app.config.ConfigSet;
 import com.ddbj.ld.common.annotation.Module;
 import com.ddbj.ld.common.constants.DistributionEnum;
+import com.ddbj.ld.data.beans.common.AccessionsBean;
+import com.ddbj.ld.data.beans.common.DBXrefsBean;
 import com.ddbj.ld.data.beans.common.DistributionBean;
 import com.ddbj.ld.data.beans.common.OrganismBean;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -131,5 +135,92 @@ public class JsonModule {
 
             log.error("Converting json to bean is failed:\t{}\t{}\t{}", message, count, json);
         }
+    }
+
+    public AccessionsBean getAccessions(final ResultSet rs) {
+        try {
+            var accession  = rs.getString("accession");
+            var submission = rs.getString("submission");
+            var status = rs.getString("status");
+            var updated = rs.getTimestamp("updated").toLocalDateTime();
+            var published = rs.getTimestamp("published").toLocalDateTime();
+            var received = rs.getTimestamp("received").toLocalDateTime();
+            var type = rs.getString("type");
+            var center = rs.getString("center");
+            var visibility = rs.getString("visibility");
+            var alias = rs.getString("alias");
+            var experiment = rs.getString("experiment");
+            var sample = rs.getString("sample");
+            var study = rs.getString("study");
+            var loaded = rs.getByte("loaded");
+            var spots = rs.getString("spots");
+            var bases = rs.getString("bases");
+            var md5sum = rs.getString("md5sum");
+            var bioSample = rs.getString("biosample");
+            var bioProject = rs.getString("bioproject");
+            var replacedBy = rs.getString("replacedby");
+            var createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+            var updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
+
+            return new AccessionsBean(
+                    accession,
+                    submission,
+                    status,
+                    updated,
+                    published,
+                    received,
+                    type,
+                    center,
+                    visibility,
+                    alias,
+                    experiment,
+                    sample,
+                    study,
+                    loaded,
+                    spots,
+                    bases,
+                    md5sum,
+                    bioSample,
+                    bioProject,
+                    replacedBy,
+                    createdAt,
+                    updatedAt
+            );
+        } catch (SQLException e) {
+            log.error("Converting accessions is failed.", e);
+
+            return null;
+        }
+    }
+
+    public DBXrefsBean getDBXrefs(
+            final ResultSet rs,
+            final String type
+    ) {
+        try {
+            var identifier = rs.getString("accession");
+
+            return new DBXrefsBean(
+                    identifier,
+                    type,
+                    this.getUrl(type, identifier)
+            );
+
+        } catch (SQLException e) {
+            log.error("Query is failed.", e);
+
+            return null;
+        }
+    }
+
+    public DBXrefsBean getDBXrefs(
+            final String identifier,
+            final String type
+    ) {
+        return new DBXrefsBean(
+                identifier,
+                type,
+                this.getUrl(type, identifier)
+        );
     }
 }
