32af8b23f166e81d9140efaa73cacb519dc303ac
tmneetse <the024and025@gmail.com>
2021-10-12 18:11:22 +0900

    [FIX]Jga→JGA

--- batch/src/main/java/com/ddbj/ld/common/constants/ActionEnum.java
+++ batch/src/main/java/com/ddbj/ld/common/constants/ActionEnum.java
@@ -6,21 +6,21 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ActionEnum {
-    REGISTER_JGA("registerJga"),
+    REGISTER_JGA("registerJGA"),
     REGISTER_ACCESSIONS("registerAccessions"),
     REGISTER_BIOPROJECT("registerBioProject"),
     REGISTER_BIOSAMPLE("registerBioSample"),
     REGISTER_SRA("registerSRA"),
     REGISTER_ALL("registerAll"),
 
-    UPDATE_JGA("updateJga"),
+    UPDATE_JGA("updateJGA"),
     UPDATE_ACCESSIONS("updateAccessions"),
     UPDATE_BIOPROJECT("updateBioProject"),
     UPDATE_BIOSAMPLE("updateBioSample"),
     UPDATE_SRA("updateSRA"),
     UPDATE_ALL("updateAll"),
 
-    VALIDATE_JGA("validateJga"),
+    VALIDATE_JGA("validateJGA"),
     VALIDATE_ACCESSIONS("validateAccessions"),
     VALIDATE_BIOPROJECT("validateBioProject"),
     VALIDATE_BIOSAMPLE("validateBioSample"),
