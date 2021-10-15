ab9e4a543dcc814a409a293b46734ddfc815799e
tmneetse <the024and025@gmail.com>
2021-10-15 12:35:30 +0900

    [ADD]BatchException

---
+++ batch/src/main/java/com/ddbj/ld/common/exception/BatchException.java
@@ -0,0 +1,11 @@
+package com.ddbj.ld.common.exception;
+
+public class BatchException extends RuntimeException {
+
+    /** default serial version */
+    private static final long serialVersionUID = 1L;
+
+    public BatchException(final String message) {
+        super(message);
+    }
+}
