f4cdf6a76bc1cd47cf61088e0804bfbd02c89769
tmneetse <the024and025@gmail.com>
2021-10-22 10:20:33 +0900

    [UPDATE]バッチのアクションを整理

--- batch/src/main/java/com/ddbj/ld/common/constants/ActionEnum.java
+++ batch/src/main/java/com/ddbj/ld/common/constants/ActionEnum.java
@@ -18,21 +18,18 @@ public enum ActionEnum {
     REGISTER_BIOPROJECT("registerBioProject"),
     REGISTER_BIOSAMPLE("registerBioSample"),
     REGISTER_SRA("registerSRA"),
-    REGISTER_ALL("registerAll"),
 
     UPDATE_JGA("updateJGA"),
     UPDATE_ACCESSIONS("updateAccessions"),
     UPDATE_BIOPROJECT("updateBioProject"),
     UPDATE_BIOSAMPLE("updateBioSample"),
     UPDATE_SRA("updateSRA"),
-    UPDATE_ALL("updateAll"),
 
     VALIDATE_JGA("validateJGA"),
     VALIDATE_ACCESSIONS("validateAccessions"),
     VALIDATE_BIOPROJECT("validateBioProject"),
     VALIDATE_BIOSAMPLE("validateBioSample"),
     VALIDATE_SRA("validateSRA"),
-    VALIDATE_ALL("validateAll"),
 
     ;
 
