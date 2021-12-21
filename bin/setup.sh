f5a52a89ee8c2eef07fa8f2d8b170b13b9f00736
tmneetse <the024and025@gmail.com>
2021-12-21 14:38:10 +0900

    [UPDATE].envのパーミッションを600に

--- bin/setup.sh
+++ bin/setup.sh
@@ -23,6 +23,7 @@ mkdir -p "data/public_db" "data/repos_db" "data/elasticsearch" "data/elasticsear
 
 chmod -R +t data
 cp -p .env.sample .env
+chmod 600 .env
 cp -p batch/src/main/resources/application.properties-sample  batch/src/main/resources/application.properties
 cp -p batch/src/main/resources/ddbj-batch.properties-sample  batch/src/main/resources/ddbj-batch.properties
 cp -p api/src/main/resources/application.properties-sample api/src/main/resources/application.properties
