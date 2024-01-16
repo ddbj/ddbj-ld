f70c7a39b6e6917a8ce848dd60f365bec1e62eb1
suecharo <suehiro619@gmail.com>
2024-01-16 17:14:54 +0900

    clamav を削除

--- bin/setup.sh
+++ bin/setup.sh
@@ -15,13 +15,13 @@ fi
 
 mkdir logs
 chmod +t logs
-mkdir -p data/public_db data/repos_db data/elasticsearch data/elasticsearch2 data/file data/clamav
+mkdir -p data/public_db data/repos_db data/elasticsearch data/elasticsearch2 data/file
 
 chmod -R +t data
 chmod 777 data/elasticsearch data/elasticsearch2
 cp -p .env.sample .env
 chmod 600 .env
-cp -p batch/src/main/resources/application.properties-sample  batch/src/main/resources/application.properties
-cp -p batch/src/main/resources/ddbj-batch.properties-sample  batch/src/main/resources/ddbj-batch.properties
+cp -p batch/src/main/resources/application.properties-sample batch/src/main/resources/application.properties
+cp -p batch/src/main/resources/ddbj-batch.properties-sample batch/src/main/resources/ddbj-batch.properties
 cp -p api/src/main/resources/application.properties-sample api/src/main/resources/application.properties
 cp -p api/src/main/resources/ddbj-api.properties-sample api/src/main/resources/ddbj-api.properties
