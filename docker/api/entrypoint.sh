1e79681df2d64ed4b7bac9f5fa0b505008b5b410
suecharo <suehiro619@gmail.com>
2024-01-17 00:51:19 +0900

    api-dev で build を background でしながら boot を同時に行っていたため、その処理をやめた

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -7,7 +7,7 @@ echo "Starting Spring Application..."
 echo "ENV:$ENV"
 
 if [ "$ENV" = "dev" ]; then
-  ./gradlew build --continuous --quiet &
+  ./gradlew build
   ./gradlew bootRun
 else
   java -Djava.security.egd=file:/dev/./urandom -jar ./build/libs/ddbj-ld-api-0.0.1-SNAPSHOT.jar
