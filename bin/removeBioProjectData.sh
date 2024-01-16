755a185553441a366c20d89e85421e202c11fff8
suecharo <suehiro619@gmail.com>
2024-01-16 18:44:46 +0900

    Staging の設定を削除

--- bin/removeBioProjectData.sh
+++ bin/removeBioProjectData.sh
@@ -3,20 +3,14 @@
 # BioProjectのデータを削除するコマンド
 # 環境にpsqlがインストールされていることが使用条件
 
-source .env > /dev/null 2>&1
+source .env >/dev/null 2>&1
 export PGPASSWORD=${PUBLIC_DB_PASSWORD}
 
 DBPort=5432
 ESPort=9200
 
-if [ ${ENV} = "Staging" ]; then
- DBPort=5433
- ESPort=9202
-fi
-
-psql -U root -h localhost -p ${DBPort}  -d ${PUBLIC_DB} > /dev/null 2>&1 << EOF
+psql -U root -h localhost -p ${DBPort} -d ${PUBLIC_DB} >/dev/null 2>&1 <<EOF
   TRUNCATE TABLE t_bioproject_biosample;
 EOF
 
-curl -X DELETE -fsSL "localhost:${ESPort}/bioproject" > /dev/null 2>&1
-
+curl -X DELETE -fsSL "localhost:${ESPort}/bioproject" >/dev/null 2>&1
--- bin/removeJgaData.sh
+++ bin/removeJgaData.sh
@@ -3,18 +3,13 @@
 # JGAのデータを削除するコマンド
 # 環境にpsqlがインストールされていることが使用条件
 
-source .env > /dev/null 2>&1
-export PGPASSWORD=${PUBLIC_DB_PASSWORD} > /dev/null 2>&1
+source .env >/dev/null 2>&1
+export PGPASSWORD=${PUBLIC_DB_PASSWORD} >/dev/null 2>&1
 
 DBPort=5432
 ESPort=9200
 
-if [ ${ENV} = "Staging" ]; then
- DBPort=5433
- ESPort=9202
-fi
-
-psql -U root -h localhost -p ${DBPort}  -d ${PUBLIC_DB} > /dev/null 2>&1 << EOF
+psql -U root -h localhost -p ${DBPort} -d ${PUBLIC_DB} >/dev/null 2>&1 <<EOF
   TRUNCATE TABLE t_jga_analysis_study;
   TRUNCATE TABLE t_jga_data_experiment;
   TRUNCATE TABLE t_jga_dataset_analysis;
@@ -24,8 +19,7 @@ psql -U root -h localhost -p ${DBPort}  -d ${PUBLIC_DB} > /dev/null 2>&1 << EOF
   TRUNCATE TABLE t_jga_experiment_study;
 EOF
 
-curl -X DELETE -fsSL "localhost:${ESPort}/jga-study" > /dev/null 2>&1
-curl -X DELETE -fsSL "localhost:${ESPort}/jga-dataset" > /dev/null 2>&1
-curl -X DELETE -fsSL "localhost:${ESPort}/jga-policy" > /dev/null 2>&1
-curl -X DELETE -fsSL "localhost:${ESPort}/jga-dac" > /dev/null 2>&1
-
+curl -X DELETE -fsSL "localhost:${ESPort}/jga-study" >/dev/null 2>&1
+curl -X DELETE -fsSL "localhost:${ESPort}/jga-dataset" >/dev/null 2>&1
+curl -X DELETE -fsSL "localhost:${ESPort}/jga-policy" >/dev/null 2>&1
+curl -X DELETE -fsSL "localhost:${ESPort}/jga-dac" >/dev/null 2>&1
